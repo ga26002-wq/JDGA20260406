7527f5bcab4490e7d16289fe64a3a39c8ce05ad5
Josue Garay <Usuario@DESKTOP-EHLDOSJ>
2026-04-07 03:44:38 -0600

    cambio de josue en AppContext.cs  se creo y modifico codigo

--- JDGA20260406.AppSeguridadWeb/Models/AppDbContext.cs
+++ JDGA20260406.AppSeguridadWeb/Models/AppDbContext.cs
@@ -1,22 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using JDGA20260406.AppSeguridadWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class AppDbContext : DbContext
-{
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    public DbSet<Usuario> Usuarios { get; set; }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class AppDbContext : DbContext
     {
-        string passwordHash = "$2a$11$hDEUK3aSHfrxt4SDvkV94.BHbSk2dfR8AX0/.puPHXqVDWcPAwyc6";  // Admin123
-        modelBuilder.Entity<Usuario>().HasData(
-            new Usuario
-            {
-                Id = 1,
-                Login = "admin",
-                PasswordHash = passwordHash,
-                Rol = "Administrador",
-                EstaActivo = true
-            }
-        );
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            string passwordHash = "$2a$11$hDEUK3aSHfrxt4SDvkV94.BHbSk2dfR8AX0/.puPHXqVDWcPAwyc6";  // Admin123
+            modelBuilder.Entity<Usuario>().HasData(
+                new Usuario
+                {
+                    Id = 1,
+                    Login = "admin",
+                    PasswordHash = passwordHash,
+                    Rol = "Administrador",
+                    EstaActivo = true
+                }
+            );
+        }
     }
-}
