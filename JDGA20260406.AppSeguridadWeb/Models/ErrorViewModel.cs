f3d0cbad4687d3e75ea1b420858e113ae6fea339
Josue Garay <Usuario@DESKTOP-EHLDOSJ>
2026-04-07 03:08:04 -0600

    Agregar archivos de proyecto.

---
+++ JDGA20260406.AppSeguridadWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JDGA20260406.AppSeguridadWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
